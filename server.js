36f4b4750119c4c3a915ac0ae66482c1cbf5f3d6
Franco Ortega <francopdx@gmail.com>
2021-12-23 13:43:05 -0800

    retrieve and log records

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const { API_KEY, BASE } = process.env;
 
 const base = new Airtable({ apiKey: API_KEY}).base(BASE);
 
-console.log(base);
+base('Maps')
+    .select({})
+    .eachPage((records, next) => {
+        records.forEach(record => console.log(record.fields));
+        next();
+    })
+    .catch(err => console.log(err));
